d42b0b77f1b7ca4a2468a7ccad5136ebf6bba534
saideepesh000 <saideepesh000@gmail.com>
2021-01-19 22:44:22 +0530

    final but initial MVP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,97 @@
-import ShowRandomLetter from './components/ShowRandomLetter';
-import UserInput from './components/UserInput';
-import Temp from './temp';
+import {useState} from 'react';
+import './App.css';
+
+const App = () => {
+	let [word, setWord] = useState("");
+	let [final, setFinal] = useState(""); 
+	const [enter, setEnter] = useState('');
+	const [score, setScore] = useState(0);
+	const [num, setNum] = useState(1);
 
 
-const App = () =>{
-  return(
-    <div>
-    <ShowRandomLetter/>
-    {/* <Temp/> */}
-    </div>
-    )
+
+	const randomWordUtil = (length) => {
+   		var result = '';
+   		var characters = 'abcdefghijklmnopqrstuvwxyz';
+   		var charactersLength = characters.length;
+	   for ( var i = 0; i < length; i++ ) {
+	      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	   }
+	   return result;
+	}
+
+	function setIntervalLimited(callback, interval, x) {
+    for (var i = 0; i < x; i++) {
+        setTimeout(callback, i*interval);
+    }
+}
+	
+	const onChange = (e) => {
+		setEnter(e.target.value);
+	}
+	let strGame;
+
+
+	const startGame = () => {
+
+
+		setNum(num => num + 1);
+		strGame = setIntervalLimited(()=>{
+			word = randomWordUtil(1);
+			setWord(word);
+			setFinal(final => final+ word);
+
+		}, 1000, num)
+
+		setWord('');
+		setFinal('');
+		clearTimeout(strGame);
+
+	}
+
+
+
+	const stopGame = () =>{
+		setWord('');
+		setFinal('');
+		setEnter('');
+		setScore(score => score=0);
+		setNum(0);
+		clearTimeout(strGame);
+	}
+		
+	const check = () =>{
+		if(enter === final){
+		setScore(score => score + 1);
+		alert(`So far it's ${score+1} letters, cmon I can make it 🔥🔥`);
+		startGame();
+		}
+		else{
+			alert(`OHHHH, I remembererd only ${score+1} letters in a word`);
+			stopGame();
+		}
+
+	}
+
+
+	return(
+		<div className="show-random-letter">
+		<div className="score-word">
+		<p className="score">{score}</p>
+		<p className="word">{word}</p>
+		</div>
+		<div className="start-stop">
+		<button className="btn btn-start" onClick={()=> { 
+			startGame();
+		}}>start</button>
+		<button className="btn btn-stop" onClick={stopGame}>Reset</button>
+		</div>
+		<div  className="check-group">
+		<input className="input" type="text" value={enter} onChange = {onChange} placeholder="Enter that letter/word"/>
+		<button className="btn btn-check" onClick={check}>Check for match</button>
+		</div>
+		</div>
+		)
 }
 
 export default App;
